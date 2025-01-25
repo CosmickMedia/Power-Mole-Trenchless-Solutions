2a06691780494815c57a639b1befd6966f019061
Anthony Ramirez <pacasoramirez@gmail.com>
2025-01-24 19:14:08 -0500

    develop, configure and stylize about section

---
+++ wp-content/themes/starterpistol-child/template-parts/quick-links-menu.php
@@ -0,0 +1,10 @@
+<div class="site-menu">
+	<nav id="site-navigation" class="navigation navigation--quick-menu">
+		<?php
+			wp_nav_menu( array(
+				'theme_location' => 'quick-menu',
+				'menu_id'        => 'quick-menu',
+			) );
+		?>
+	</nav><!-- #site-navigation -->
+</div>
---
+++ wp-content/themes/starterpistol-child/template-parts/section-about.php
@@ -0,0 +1,41 @@
+<?php
+	$about_image = get_stylesheet_directory_uri().'/images/yellow-truck.jpg';
+?>
+
+<div class="section section--quick-menu">
+	<?php get_template_part('template-parts/quick-links-menu'); ?>
+	
+	<div class="section section--about">
+		<div class="l-constrain">
+			<div class="columns">
+				<div class="column-half media">
+					<img src="<?php echo $about_image; ?>">
+				</div>
+				<div class="column-half content">
+					<h2><span>Solutions</span> for All Trenchless Technologies</h2>
+					<subtitle>Serving the Industry for over 60 years</subtitle>
+					
+					<p>Embrace the many benefits of trenchless technology by turning to Pow-R Mole Trenchless Solutions for supplies and equipment you need to complete trenchless projects. For more than six decades, we've been making high-quality equipment designed to boost efficiency and productivity for plumbers, contractors, developers, and other professionals.</p>
+
+					<p>From pipe fusion equipment to utility locators, there are many pieces of equipment needed to take full advantage of trenchless technology. We offer a wide assortment of top-quality trenchless supplies.</p>
+					
+					<a href="#" class="button button--accent-color">Contact Us For a Free Quote</a>
+				</div>
+			</div>
+			
+			<div class="bottom">
+				<div class="columns columns--vertical-center">
+					<div class="column-fourths is-title">
+						<h3><span>5-Year</span> Manufacturer Warranty</h3>
+					</div>
+					<div class="column-half is-content">
+						<p>Our 5 year warranty, along with the attention of our factory-trained technicians, will help you get the most out of your equipment.</p>
+					</div>
+					<div class="column-fourths is-cta">
+						<a href="#" class="button button--transparent">Contact Us For Details</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
