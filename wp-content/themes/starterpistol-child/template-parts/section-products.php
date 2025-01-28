3e35c1293c01658e13e7ecff2b947b2fb2801dae
Anthony Ramirez <pacasoramirez@gmail.com>
2025-01-28 18:01:57 -0500

    Product Section stylization, and content integration

--- wp-content/themes/starterpistol-child/template-parts/section-products.php
+++ wp-content/themes/starterpistol-child/template-parts/section-products.php
@@ -1,5 +1,8 @@
 <div class="section section--products">
 	<div class="l-constrain">
+		<h2>First-Rate Trenchless <span>Solutions</span></h2>
+		<p>Our trenchless products help solve critical problems throughout all phases of the trenchless process, from the initial inspection through completion. Whether you only need a few pieces or a longer list of supplies, we've got you covered. Here's what we offer:</p>
+
 		<?php echo do_shortcode('[products]'); ?>
 	</div>
 	
