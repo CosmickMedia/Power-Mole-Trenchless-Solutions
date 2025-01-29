13810f11d42f752b8a63d133d562fe428860d52b
Anthony Ramirez <pacasoramirez@gmail.com>
2025-01-29 13:07:49 -0500

    section cta development and styling

---
+++ wp-content/themes/starterpistol-child/template-parts/section-cta.php
@@ -0,0 +1,22 @@
+<?php
+	$pmts_logo = get_stylesheet_directory_uri().'/images/PMTS-mole-only.svg';
+?>
+
+<div class="section section--cta">
+	<div class="inner">
+		<div class="columns columns--vertical-center">
+			<div class="column-half media columns">
+				<img src="<?php echo $pmts_logo; ?>">
+				<h2>Ready to <span>upgrade</span> your trenchless equipment?</h2>
+			</div>
+			<div class="column-half content">
+				<p>Pow-R Mole Trenchless offers top quality products designed to solve even the most complex trench and pipeline problems.</p>
+
+				<div class="button-group">
+					<a href="#" class="button button--accent-color button--small">Become a Dealer</a>
+					<a href="tel:<?php echo esc_html(the_field('phone', 'options')); ?>" class="button button--invert button--small">Call <?php echo esc_html(the_field('phone', 'options')); ?></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
