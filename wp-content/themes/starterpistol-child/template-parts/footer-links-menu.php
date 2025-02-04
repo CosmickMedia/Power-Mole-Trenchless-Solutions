823c3033528853a39f353a23b682838ffca98430
Anthony Ramirez <pacasoramirez@gmail.com>
2025-02-04 15:33:19 -0500

    Added Quick Links, Product Menus, and Social Media Link styling

---
+++ wp-content/themes/starterpistol-child/template-parts/footer-links-menu.php
@@ -0,0 +1,10 @@
+<div class="site-menu">
+	<nav id="site-navigation" class="navigation navigation--footer-links">
+		<?php
+			wp_nav_menu( array(
+				'theme_location' => 'footer-links',
+				'menu_id'        => 'footer-links',
+			) );
+		?>
+	</nav><!-- #site-navigation -->
+</div>
